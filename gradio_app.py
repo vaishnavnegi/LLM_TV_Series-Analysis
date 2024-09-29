5faf251ff0b848fda016ee06dacd8a7fa6409eb7
vaishnavnegi <vaishnavnegi207@gmail.com>
2024-09-29 23:17:07 +0200

    Minor changes

--- gradio_app.py
+++ gradio_app.py
@@ -53,7 +53,7 @@ def classify_text(text_classifcation_model,text_classifcation_data_path,text_to_
     return output
 
 def chat_with_character_chatbot(message, history):
-    character_chatbot = CharacterChatBot("LukeVaine/Naruto_Llama-3.1-8B-Instruct_chatbot",
+    character_chatbot = CharacterChatBot("LukeVaine/Naruto_Llama-3-8B_2",
                                          huggingface_token = os.getenv('huggingface_token')
                                          )
 
